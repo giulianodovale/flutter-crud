d5f59d2b67c2d5cade911731f8125e782d9d866a
giulianodovale <giulianodovale@hotmail.com>
2020-11-17 22:56:01 -0300

    base para projeto app ministerior de louvor

---
+++ lib/data/dummy_user.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  "1": const User(
+      id: '1',
+      name: "Giuliano 1",
+      email: 'giulianodovale@gmail.com',
+           avatarUrl:'https://media.istockphoto.com/vectors/male-avatar-profile-picture-vector-id817481430'),
+  "2": const User(
+      id: '2',
+      name: "Jose 2",
+      email: 'jose@gmail.com',
+      avatarUrl:'https://media.istockphoto.com/vectors/male-avatar-profile-picture-vector-id817481430'),
+  "3": const User(
+      id: '3',
+      name: "Maria 3",
+      email: 'maria@gmail.com',
+      avatarUrl:
+          'https://media.istockphoto.com/vectors/female-avatar-profile-picture-vector-id817481360'),
+  "4": const User(
+      id: '4',
+      name: "Joao  4",
+      email: 'joao@gmail.com',
+      avatarUrl:'https://media.istockphoto.com/vectors/male-avatar-profile-picture-vector-id817481430')
+      };
